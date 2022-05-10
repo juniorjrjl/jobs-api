21c6a2b9c6e3abf018ac81d16a77e4659f0aa252
José Luiz Abreu Cardoso Junior <junior@MacBook-Pro-de-Jose.local>
2022-05-10 12:28:30 -0300

    endpoints de companies

--- src/controllers/companies-controler.ts
+++ src/controllers/companies-controler.ts
@@ -11,5 +11,71 @@ export const companiesController = {
                 return res.status(400).json({message: err.message})
             }
         }
-    }
+    },
+
+    //POST /companies
+    save: async (req: Request, res: Response) =>{
+        const { name, bio, website, email } = req.body
+        try{
+            const companies = await Company.create({ 
+                name, 
+                bio, 
+                website, 
+                email
+            })
+            return res.status(201).json(companies)
+        }catch(err){
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //GET /companies/:id
+    show: async (req: Request, res: Response) =>{
+        const { id } = req.params
+        try{
+            const company = await Company.findByPk(id)
+            return res.json(company)
+        }catch(err){
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //PUT /companies/:id
+    update: async (req: Request, res: Response) =>{
+        const { id } = req.params
+        const { name, bio, website, email } = req.body
+        try{
+            const [affectedRows, companies] = await Company.update({ 
+                name, 
+                bio, 
+                website, 
+                email
+            }, {
+                where: { id },
+                returning: true})
+            return res.json(companies[0])
+        }catch(err){
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //GET /companies/:id
+    delete: async (req: Request, res: Response) =>{
+        const { id } = req.params
+        try{
+            await Company.destroy({where: { id }})
+            res.status(204).send()
+        }catch(err){
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -13,5 +13,9 @@ router.put('/candidates/:id', candidatesController.update)
 router.delete('/candidates/:id', candidatesController.delete)
 
 router.get('/companies', companiesController.index)
+router.get('/companies/:id', companiesController.show)
+router.post('/companies', companiesController.save)
+router.put('/companies/:id', companiesController.update)
+router.delete('/companies/:id', companiesController.delete)
 
 export { router }
