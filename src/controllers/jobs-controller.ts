1bf2167c9b4ab024d3e1f79ded9786741ca20d06
José Luiz Abreu Cardoso Junior <junior@MacBook-Pro-de-Jose.local>
2022-05-11 14:36:27 -0300

    endpoints de gerenciamento de jobs

--- src/controllers/jobs-controller.ts
+++ src/controllers/jobs-controller.ts
@@ -12,6 +12,73 @@ export const jobsController ={
                 return res.status(400).json({message: err.message})
             }
         }
+    },
+
+    //POST /jobs
+    save: async (req: Request, res: Response) => {
+        const { title, description, limitDate, companyId} = req.body
+        try {
+            const job = await Job.create({
+                title,
+                description,
+                limitDate,
+                companyId
+            })
+
+            return res.status(201).json(job)
+        } catch (err) {
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //GET /jobs/:id
+    show: async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            const job = await Job.findByPk(id, { include: 'company' })
+            return res.json(job)
+        } catch (err) {
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //PUT /jobs/:id
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params
+        const { title, description, limitDate, companyId} = req.body
+        try {
+            const [affectedRows, jobs] = await Job.update({
+                title,
+                description,
+                limitDate,
+                companyId
+            }, { 
+                where: {id},
+                returning: true
+            })
+            return res.json(jobs[0])
+        } catch (err) {
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
+    },
+
+    //DELETE /jobs/:id
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params
+        try {
+            await Job.destroy({ where: {id} })
+            return res.status(204).send()
+        } catch (err) {
+            if (err instanceof Error){
+                return res.status(400).json({message: err.message})
+            }
+        }
     }
 }
 
